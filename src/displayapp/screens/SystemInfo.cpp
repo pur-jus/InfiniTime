29d04e5349f2bb1de4bc051253d9fc1ff73cfd03
pur-jus <lienversju@gmail.com>
2021-07-21 16:09:26 +0200

    Add a system info to indicate the purpose of this demo.

--- src/displayapp/screens/SystemInfo.cpp
+++ src/displayapp/screens/SystemInfo.cpp
@@ -83,7 +83,8 @@ std::unique_ptr<Screen> SystemInfo::CreateScreen1() {
   lv_obj_t* label = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_recolor(label, true);
   lv_label_set_text_fmt(label,
-                        "#FFFF00 InfiniTime#\n\n"
+                        "#FFFF00 InfiniTime#\n"
+                        "#FFFF00 smart home demo#\n"
                         "#444444 Version# %ld.%ld.%ld\n"
                         "#444444 Short Ref# %s\n"
                         "#444444 Build date#\n"
