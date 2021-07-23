804e53f8345ee2d8f16e96a4c546c736c97b4c79
pur-jus <lienversju@gmail.com>
2021-07-23 10:42:03 +0200

    Update system info.

--- src/displayapp/screens/SystemInfo.cpp
+++ src/displayapp/screens/SystemInfo.cpp
@@ -84,7 +84,7 @@ std::unique_ptr<Screen> SystemInfo::CreateScreen1() {
   lv_label_set_recolor(label, true);
   lv_label_set_text_fmt(label,
                         "#FFFF00 InfiniTime#\n"
-                        "#FFFF00 smart home demo#\n"
+                        "#FFFF00 Smatchaus#\n"
                         "#444444 Version# %ld.%ld.%ld\n"
                         "#444444 Short Ref# %s\n"
                         "#444444 Build date#\n"
